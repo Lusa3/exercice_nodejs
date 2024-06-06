4b9602095294cf85d2ddb7f9ec53c68723a3cb7c
grace luswanga <grace_lusa@MBP-des-filles.local>
2024-06-06 14:08:13 +0100

    ajout de fonctionalité js

--- index.js
+++ index.js
@@ -7,4 +7,31 @@ app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) =>{
     res.render("index");
+});
+app.get("/contact", (req, res) =>{
+    res.render("contact")
+});
+app.get("/about",(req, res) => {
+    res.render("about")
+});
+app.get("/articles", (req,res) =>{
+    res.render("articles",{articles})
+});
+app.get("/articles/:slug", (req, res) =>{
+    const {slug} = rq.params;
+    const article = articles.find(article => article.slug == slug);
+    if(article){
+        res.render("article",{article})
+    } else{
+        res.render("404")
+    }
 })
+app.get("/*", (req, res)=>{
+    res.render("404")
+})
+const port = 3002;
+
+app.listen(port, function () {
+  console.log(`l'application ecoute sur le port ${port}`);
+  console.log(`l'application est disponible sur http://localhost:${port}`);
+});
